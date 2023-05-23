9e88958d3a433415e9b80613c7c6c092365cdccb
Husu Bianca <126001578+HusuBia@users.noreply.github.com>
2023-05-23 12:05:21 +0300

    Update MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,7 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+
 
 namespace Durere_cu_poneii
 {
@@ -25,4 +27,45 @@ namespace Durere_cu_poneii
             InitializeComponent();
         }
     }
+     private async Task RunGame()
+        {
+            Draw();
+            await ShowCountDown();
+            Overlay.Visibility = Visibility.Hidden;
+            await GameLoop();
+            await ShowGameOver();
+            gameState = new GameState(rows, cols);
+
+        private async void Window_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (Overlay.Visibility == Visibility.Visible)
+            {
+                e.Handled = true;
+            }
+            if (!gameRunning)
+            {
+                gameRunning = true;
+                await RunGame();
+                gameRunning = false;
+            }
+        }
+        
+        private async Task GameLoop()
+        {
+            while (!gameState.GameOver)
+            {
+                await Task.Delay(100);
+                gameState.Move();
+                Draw();
+            }
+            }
+            private void Draw()
+        {
+            DrawGrid();
+            DrawPoneiHead();
+            ScoreText.Text = $"SCORE: {gameState.Score}";
+        }
+        
+
+
 }
