60e2a99f03f9406cd3cda7841a4103514b00776f
CaplatMiruna <73909640+CaplatMiruna@users.noreply.github.com>
2023-05-23 12:07:40 +0300

    Update MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -66,6 +66,42 @@ namespace Durere_cu_poneii
             ScoreText.Text = $"SCORE: {gameState.Score}";
         }
         
+        private void DrawPoneiHead()
+        {
+            Position headPos = gameState.HeadPosition();
+            Image image = gridImages[headPos.Row, headPos.Col];
+            image.Source = Images.Head;
 
+            int rotation = dirToRotation[gameState.Dir];
+            image.RenderTransform = new RotateTransform(rotation);
+        }
+        private async Task DrawDeadPonei()
+        {
+            List<Position> positions = new List<Position>(gameState.PoneiPositions());
+            for (int i = 0; i < positions.Count; i++)
+            {
+                Position pos = positions[i];
+                ImageSource source = (i == 0) ? Images.DeadHead : Images.DeadBody;
+                gridImages[pos.Row, pos.Col].Source = source;
+                await Task.Delay(50);
+            }
+        }
+
+        private async Task ShowCountDown()
+        {
+            for (int i = 3; i >= 1; i--)
+            {
+                OverlayText.Text = i.ToString();
+                await Task.Delay(500);
+            }
+        }
+        private async Task ShowGameOver()
+        {
+            await DrawDeadPonei();
+            await Task.Delay(1000);
+            Overlay.Visibility = Visibility.Visible;
+            OverlayText.Text = "Try again";
+        }
+    }
 
 }
